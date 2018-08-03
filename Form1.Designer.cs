c2484e97311ad403c09acff7bfc3e50a710a1376
moauris <mchenf@icloud.com>
2018-08-03 19:06:43 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace nmon_proc_windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        public void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(124, 19);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(92, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 544);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(455, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.DataSource = this.chart1.Images;
+            legend1.LegendStyle = LegendStyle.Row;
+            legend2.LegendStyle = LegendStyle.Row;
+            legend3.LegendStyle = LegendStyle.Row;
+            legend1.Docking = Docking.Top;
+            legend2.Docking = Docking.Top;
+            legend3.Docking = Docking.Top;
+            legend1.Name = "Legend1";
+            legend2.Name = "Legend2";
+            legend3.Name = "Legend3";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(12, 148);
+            this.chart1.Name = "chart1";
+            this.chart1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.IsVisibleInLegend = false;
+            series2.Legend = "Legend2";
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.IsVisibleInLegend = false;
+            series3.Legend = "Legend3";
+            series3.Name = "Series3";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(776, 379);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "NMON_CHART";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(366, 129);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Process Nmon Files";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(422, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(366, 129);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Nmon Data Visualization";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(7, 67);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(50, 17);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "IOPS";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(227, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Show Graph";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(7, 44);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(47, 17);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "CPU";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(76, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "MEMNEW";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 576);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private Chart chart1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private RadioButton radioButton1;
+        private Button button2;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using nmonproc;
+
+namespace nmon_proc_windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime dt1 = dateTimePicker1.Value;
+
+            //MessageBox.Show(dt1.ToString("yyyy-MM-dd"));
+            try
+            {
+                Proc_nmon.Extract(dt1, @"c:\perfdata\");
+            }
+            catch
+            {
+                textBox1.Text = "File Not Found.";
+                return;
+            }
+            XDocument xd = 
+                XDocument.Load(@"c:\perfdata\nmon_perf_database.xml");
+            //textBox1.Text = (xd.ToString());
+            int days_cnt = xd.Elements("NMON_RECORDS").Elements().Count();
+            textBox1.Text = String.Format("{0} days in database", days_cnt);
+        }
+        /*
+        private void button2_Click(object sender, EventArgs e)
+        {//read from xml database, and show graph
+            string q_opt = "";
+            RadioButton[] rbn = { radioButton1, radioButton3, radioButton4 };
+            foreach (RadioButton r in rbn)
+            {
+                if (r.Checked == true) q_opt = r.Text;
+            }
+            textBox1.Text = $"{q_opt} is selected.";
+            XDocument nmon_db = XDocument.Load
+                (@"C:\perfdata\nmon_perf_database.xml");
+
+            IEnumerable<XElement> xe = 
+                nmon_db.Elements("NMON_RECORDS").Elements();
+
+            foreach (XElement el in xe)
+            {
+                chart1.Series["series1"]
+                    .Points.AddXY
+                    (el.Name.ToString(), 
+                    Convert.ToDouble
+                    (el.Element($"{q_opt}_MAX").Value));
+            }
+        }*/
+        private void button2_Click(object sender, EventArgs e)
+        {//read from xml database, and show graph
+            for (int i = 0; i<3; i++)
+                chart1.Series[i].Points.Clear();
+            string q_opt = "";
+            string Xax; double Yax;
+            RadioButton[] rbn = { radioButton1, radioButton3, radioButton4 };
+            foreach (RadioButton r in rbn)
+            {
+                if (r.Checked == true) q_opt = r.Text;
+            }
+            textBox1.Text = $"{q_opt} is selected.";
+            XDocument nmon_db = XDocument.Load
+                (@"C:\perfdata\nmon_perf_database.xml");
+
+            IEnumerable<XElement> xe =
+                nmon_db.Elements("NMON_RECORDS").Elements();
+
+            if (q_opt == "MEMNEW")
+            {
+                chart1.Series[0].Name = "PROCESS_MAX";
+                chart1.Series[1].Name = "FSCACHE_MAX";
+                chart1.Series[2].Name = "SYSTEM_MAX";
+                chart1.Series[0].IsVisibleInLegend = true;
+                chart1.Series[1].IsVisibleInLegend = true;
+                chart1.Series[2].IsVisibleInLegend = true;
+                foreach (XElement el in xe)
+                {
+                    Xax = el.Name.ToString();
+                    Yax = Convert.ToDouble(el.Element($"{q_opt}").Attribute("PROCESS_MAX").Value);
+                    chart1.Series[0].Points.AddXY(Xax, Yax);
+                    Yax = Convert.ToDouble(el.Element($"{q_opt}").Attribute("FSCACHE_MAX").Value);
+                    chart1.Series[1].Points.AddXY(Xax, Yax);
+                    Yax = Convert.ToDouble(el.Element($"{q_opt}").Attribute("SYSTEM_MAX").Value);
+                    chart1.Series[2].Points.AddXY(Xax, Yax);
+                }
+            }
+            else if (q_opt == "IOPS" || q_opt == "CPU")
+            {
+                chart1.Series[0].Name = $"{q_opt}_MAX";
+                chart1.Series[0].IsVisibleInLegend = true;
+                chart1.Series[1].IsVisibleInLegend = false;
+                chart1.Series[2].IsVisibleInLegend = false;
+                foreach (XElement el in xe)
+                {
+                    //MessageBox.Show(chart1.Series[0].Name);
+                    Xax = el.Name.ToString();
+                    Yax = Convert.ToDouble(el.Element($"{q_opt}_MAX").Value);
+                    chart1.Series[0]
+                        .Points.AddXY(Xax, Yax);
+                    //Debug//File.AppendAllText("log.txt", $"{Xax}\t{Yax}\r\n");
+
+                }
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
